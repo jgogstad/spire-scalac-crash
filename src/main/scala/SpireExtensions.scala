25c139c518d40654e669fd51383ec11fe8a4c9c2
Jostein Gogstad <jostein.gogstad@gmail.com>
2023-12-20 20:37:35 +0100

    add comment

--- src/main/scala/SpireExtensions.scala
+++ src/main/scala/SpireExtensions.scala
@@ -6,5 +6,6 @@ extension [A: spire.math.Integral] (a: A) {
 }
 
 extension [A: spire.math.Integral] (n: Bounded[A]) {
+  // this code isn't actually correct as far as calculating sizes goes as it doesn't account for closed and open bounds
   def size: Long = spire.math.Integral[A].minus(n.upperBound.a, n.lowerBound.a).coerce
 }
